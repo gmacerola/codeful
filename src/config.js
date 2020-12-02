767a70aa1661ae62f113e5a07f514ca69447bb1c
Gianpaolo Macerola <gmacerola@gmail.com>
2020-12-02 16:33:33 -0600

    added / to url

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   TOKEN_KEY: process.env.TOKEN_KEY || "codeful-token",
   DATABASE_URL:
-    process.env.DATABASE_URL || "https://agile-brushlands-42217.herokuapp.com",
+    process.env.DATABASE_URL || "https://agile-brushlands-42217.herokuapp.com/",
 };
